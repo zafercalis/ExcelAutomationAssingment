04922f1346431f0e782679b634aba8bd4c02d2a2
zafercalis <zaferecalis@gmail.com>
2017-09-07 12:42:53 -0400

    additional calculaitons by zafer

--- src/test/java/com/excel/tests/CalculatorTest.java
+++ src/test/java/com/excel/tests/CalculatorTest.java
@@ -16,6 +16,10 @@ public class CalculatorTest {
 
 		System.out.println(Calculator.Operate("division", 3.5, 0));
 
+		System.out.println(Calculator.Operate("addition",7, 2.1));
+
+		System.out.println(Calculator.Operate("subtraction", 3.5, 0));
+		// Changes made by Zafer on 9/7/2017
 		
 	}
 
