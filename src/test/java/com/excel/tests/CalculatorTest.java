21e61c2ee3844484d9cf365860d0608fba973307
naji90 <naji90cr7@gmail.com>
2017-09-07 13:22:06 -0400

    update from hussein

--- src/test/java/com/excel/tests/CalculatorTest.java
+++ src/test/java/com/excel/tests/CalculatorTest.java
@@ -3,40 +3,36 @@ package com.excel.tests;
 import org.junit.Test;
 
 public class CalculatorTest {
-	
-	
+
 	@Test
-	public void testMyCalculator(){
-		
+	public void testMyCalculator() {
+
 		System.out.println(Calculator.Operate("addition", 3.5, 2.1));
-		
+
 		System.out.println(Calculator.Operate("subtraction", 678, 2.1));
-		
-		System.out.println(Calculator.Operate("multiplication",7, 2.1));
+
+		System.out.println(Calculator.Operate("multiplication", 7, 2.1));
 
 		System.out.println(Calculator.Operate("division", 3.5, 0));
 
-		System.out.println(Calculator.Operate("addition",7, 2.1));
+		System.out.println(Calculator.Operate("addition", 7, 2.1));
 
 		System.out.println(Calculator.Operate("subtraction", 3.5, 0));
 		// Changes made by Zafer on 9/7/2017
 		// just for practice
 		//
-		
-		
-		
-		
-		
-		//let's try again 
-		
+
+		// let's try again
+
 		/*
 		 * we like to collaborate
 		 */
 
-		//practice is good always
-		//still trying here :)
+		// practice is good always
+		// still trying here :)
 		System.out.println("dilek change");
 		System.out.println("second try from Dilek");
+		System.out.println("hussein");
 	}
 
 }
